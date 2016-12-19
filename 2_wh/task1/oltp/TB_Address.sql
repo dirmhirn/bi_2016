1c9358cc54fbfe280d08a5e71cf509f02c8ea762
dirmhirn <dirmhirn@gmx.at>
2016-12-19 21:55:46 +0100

    switch postalcode

--- 2_wh/task1/oltp/TB_Address.sql
+++ 2_wh/task1/oltp/TB_Address.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `TB_Address` (
   `AddressLine` VARCHAR(255) NOT NULL,
   `City` VARCHAR(45) NOT NULL,
   `StateProvince` VARCHAR(45),
-  `PostalCode` VARCHAR(45) NOT NULL,
   `CountryRegion` VARCHAR(45) NOT NULL,
+  `PostalCode` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`AddressID`))
 ENGINE = InnoDB;
